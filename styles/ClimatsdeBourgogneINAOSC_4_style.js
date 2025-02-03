bc417c3b0393c171f3ebd9f9ac4353a4fa0bf1d5
CABCS021 <101575781+CABCS021@users.noreply.github.com>
2025-02-03 15:47:55 +0100

    Update ClimatsdeBourgogneINAOSC_4_style.js

--- styles/ClimatsdeBourgogneINAOSC_4_style.js
+++ styles/ClimatsdeBourgogneINAOSC_4_style.js
@@ -6,7 +6,7 @@ function categories_ClimatsdeBourgogneINAOSC_4(feature, value, size, resolution,
                 var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
                 switch(valueStr) {case 'Grand Cru':
                     return [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(247,247,247,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(173,42,48,0.5019607843137255)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(173,42,48,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.2}),fill: new ol.style.Fill({color: 'rgba(173,42,48,0.5019607843137255)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
